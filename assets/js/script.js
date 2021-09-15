e9022f334fd01a37970deba8a649915161dad7ff
David Bowers <dnlbowers@gmail.com>
2021-09-16 00:40:03 +0200

    Add function names and docs strings for skeleton of the script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+/**
+ * Deals a random card on to the table.
+ * This card will be assigned to the player or the house during playRound()
+ */
+function dealCard() {
+//This is where my arrays will go
+}
+
+/**
+ * Checks for blackjack on initial hand dealt
+ */
+function checkBlackjack (hand) {
+
+}
+
+/**
+ * Checks the hand value and offers to convert the value of ace from high to low.
+ */
+function checkHandValue(hand){
+
+}
+
+/**
+ * Flips the houses hidden card face up once the players turn is over
+ */
+function houseReveal(){
+
+}
+
+/**
+ * Compares final hands once both the house and the player have drawn all their desired cards
+ */
+function compareHands(playerHand, houseHand) {
+
+}
+
+
