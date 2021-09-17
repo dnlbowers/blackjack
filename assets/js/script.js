73f126ebcf711775839ae017b0e2bb2de3e60232
David Bowers <dnlbowers@gmail.com>
2021-09-17 11:57:22 +0200

    Create img element within dealCard function for the random card and send to the correct container in the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 const dealerContainer = document.getElementById('dealer-card-container');
 const playerContainer = document.getElementById('player-card-container');
 
+dealCard('player');
+dealCard('dealer')
 
 /**
  * Deals a random card on to the table.
  * This card will be assigned to the player or the house during playRound()
  */
-function dealCard() {
+function dealCard(dealtFor) {
 
 let suitArray = ['hearts', 'clubs', 'spades', 'diamonds'];
 
@@ -18,9 +20,16 @@ let suit = suitArray[randomSuit];
 let value = valueArray[randomValue];
 
 // here I would use document.createElement('img/div') and place the card down within it 
-// file path for src would look something like this src="assets/images/suit/value"
-// value to be converted into int using if statement or maybe a switch (case 'king' value = 10) I think I can leave off default?
+let card = document.createElement('img');
+card.src = `assets/images/${suit}/${value}`;
+card.alt = `${value} of ${suit}`;
 
+if (dealtFor === 'player') {
+    playerContainer.appendChild(card);
+} else if (dealtFor === 'dealer') {
+    dealerContainer.appendChild(card)
+}
+// value to be converted into int using if statement or maybe a switch (case 'king' value = 10) I think I can leave off default?
 //then this function will ditch the suit and return only the card value which will be added to an array of player/house hand
 return value; 
 }
