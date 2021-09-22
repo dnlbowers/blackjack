1a73c2ed1de56dbf6e27ca17af85cb556a465e32
David Bowers <dnlbowers@gmail.com>
2021-09-22 19:04:58 +0200

    move query selectors out of the DOM loaded event and make the DOM loaded evernt include all code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,79 +5,88 @@
 //add functionality to the menu
 //write the game rules
 //write win streak function with ability to reset
-// !! above the MVP plan !! 
+// !! above the MVP plan !!
 // add sound
 //add color choice for the game table
+const hitBtnRef = document.querySelector("#hit-btn");
+const standBtnRef = document.querySelector("#stand-btn");
 
-
-const hitBtnRef = document.querySelector('#hit-btn');
-const standBtnRef = document.querySelector('#stand-btn')
-hitBtnRef.addEventListener('click', function(){
+document.addEventListener("DOMContentLoaded", function () {
+  hitBtnRef.addEventListener("click", function () {
     if (checkHandValue(playerHand) >= 21) {
-        return
+      return;
     }
-    playerHand.push(dealCard('player'))
-})
-standBtnRef.addEventListener('click', computerTurn);
-let playerHand = [];
-let dealerHand = [];
-initializeGameRound()
+    playerHand.push(dealCard("player"));
+  });
 
+  standBtnRef.addEventListener("click", computerTurn);
+  let playerHand = [];
+  let dealerHand = [];
 
-function computerTurn() {
+  initializeGameRound();
+
+  function computerTurn() {
     hitBtnRef.disabled = true;
     let playerTotal = checkHandValue(playerHand);
     let dealerTotal = checkHandValue(dealerHand);
     console.log(playerTotal);
     console.log(dealerTotal);
-    if (dealerTotal > 17){
-        let result = compareHands(playerTotal, dealerTotal)
-        console.log(result)
+    if (dealerTotal > 17) {
+      let result = compareHands(playerTotal, dealerTotal);
+      console.log(result);
     }
-    
-    while (dealerTotal !== 0 && dealerTotal < 17){
-        dealerHand.push(dealCard('dealer'));
-        let dealerTotal = checkHandValue(dealerHand);
-        if (dealerTotal > 17){
-            let result = compareHands(playerTotal, dealerTotal)
-            console.log(result)
-            break;
-        }
+
+    while (dealerTotal !== 0 && dealerTotal < 17) {
+      dealerHand.push(dealCard("dealer"));
+      let dealerTotal = checkHandValue(dealerHand);
+      if (dealerTotal > 17) {
+        let result = compareHands(playerTotal, dealerTotal);
+        console.log(result);
+        break;
+      }
     }
-}
+  }
 
-function initializeGameRound() {
-    let playerHand = [];
-    let dealerHand = [];
+  function initializeGameRound() {
 
-    document.addEventListener('DOMContentLoaded', function(){
-        for (i = 0; i < 2; i++){
-            playerHand.push(dealCard('player'));
-            dealerHand.push(dealCard('dealer'));
-        } 
-        
-        let playerTotal = checkHandValue(playerHand);
-        let dealerTotal = checkHandValue(dealerHand);
-        console.log(playerTotal);
-        console.log(dealerTotal);
-        
-        if (playerTotal === 0 || dealerTotal === 0){
-            //Need a pop up box asking to play again and declaring score.
-            let result = compareHands(playerTotal, dealerTotal)
-            console.log(result)
+    for (i = 0; i < 2; i++) {
+      playerHand.push(dealCard("player"));
+      dealerHand.push(dealCard("dealer"));
+    }
 
-        }
-    })
-}
+    let playerTotal = checkHandValue(playerHand);
+    let dealerTotal = checkHandValue(dealerHand);
+    console.log(playerTotal);
+    console.log(dealerTotal);
 
-/**
- * Deals a random card on to the table and assigns it a value.
- * places and image of the card in DOM according to the parameter passed.
- */
-function dealCard(dealtFor) {
+    if (playerTotal === 0 || dealerTotal === 0) {
+      //Need a pop up box asking to play again and declaring score.
+      let result = compareHands(playerTotal, dealerTotal);
+      console.log(result);
+    }
+  }
 
-    let suitArray = ['hearts', 'clubs', 'spades', 'diamonds'];
-    let valueArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+  /**
+   * Deals a random card on to the table and assigns it a value.
+   * places and image of the card in DOM according to the parameter passed.
+   */
+  function dealCard(dealtFor) {
+    let suitArray = ["hearts", "clubs", "spades", "diamonds"];
+    let valueArray = [
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      "jack",
+      "queen",
+      "king",
+      "ace",
+    ];
 
     let randomSuit = Math.floor(Math.random() * 4);
     let randomValue = Math.floor(Math.random() * 13);
@@ -85,94 +94,85 @@ function dealCard(dealtFor) {
     let suit = suitArray[randomSuit];
     let value = valueArray[randomValue];
 
-    // Create <img> with attributes to visually represent the value of the card in the DOM 
-    let card = document.createElement('img');
+    // Create <img> with attributes to visually represent the value of the card in the DOM
+    let card = document.createElement("img");
     card.src = `assets/images/${suit}/${value}.svg`;
-    card.className = 'card';
+    card.className = "card";
     card.alt = `${value} of ${suit}`;
 
     //Assigns the card image to the appropriate hand according to the parameter passed.
-    if (dealtFor === 'player') {
-        document.getElementById('player-card-container').appendChild(card);
-        
-    } else if (dealtFor === 'dealer') {
-        document.getElementById('dealer-card-container').appendChild(card)
+    if (dealtFor === "player") {
+      document.getElementById("player-card-container").appendChild(card);
+    } else if (dealtFor === "dealer") {
+      document.getElementById("dealer-card-container").appendChild(card);
     }
 
     // Returns picture cards as numerical values
-    if (value === 'jack' || value === 'queen' || value === 'king') {
-        value = 10;
-        
-        return value
-    } else if (value === 'ace') {
-        value = 11;
-        
-        return value
-        
+    if (value === "jack" || value === "queen" || value === "king") {
+      value = 10;
+
+      return value;
+    } else if (value === "ace") {
+      value = 11;
+
+      return value;
     } else {
-        
-        return value    
-        
+      return value;
+    }
+  }
+
+  /**
+   * Checks the hand value for blackjack and then loops through array to total score
+   * if Ace found user will be prompted to decide if they want ace to = 1 or 11. 11(default)
+   */
+  function checkHandValue(hand) {
+    console.log(hand);
+    let handValue = 0;
+    for (card of hand) {
+      handValue += card;
     }
 
-}
+    if (handValue === 21 && hand.Length <= 2) {
+      console.log(hand.Length);
+      return 0;
+    } else {
+      return handValue;
+      //add for of loop with if card === 11 confirm box "want to convert ace hig or low"
+      // event listener on pop up buttons
+      //handValue += response from confirmation pop up (1 for low 11 for high)
+      //else
+      //handValue += card;
+    }
+  }
 
-/**
- * Checks the hand value for blackjack and then loops through array to total score
- * if Ace found user will be prompted to decide if they want ace to = 1 or 11. 11(default)
- */
-function checkHandValue(hand){
-console.log(hand)    
-let handValue = 0;
-for (card of hand) {
-   handValue += card;
-}
+  /**
+   * Flips the houses hidden card face up once the players turn is over
+   */
+  function houseReveal() {}
 
-if (handValue === 21 && hand.Length <= 2){
-    console.log(hand.Length)
-    return 0;
-} else { 
-    return handValue;
- //add for of loop with if card === 11 confirm box "want to convert ace hig or low"
-    // event listener on pop up buttons
-    //handValue += response from confirmation pop up (1 for low 11 for high)
-    //else
-    //handValue += card;
-}
-}
-
-/**
- * Flips the houses hidden card face up once the players turn is over
- */
-function houseReveal(){
-
-}
-
-/**
- * Compares final hands once both the house and the player have drawn all their desired cards
- */
-function compareHands(playerHandValue, houseHandValue) {
+  /**
+   * Compares final hands once both the house and the player have drawn all their desired cards
+   */
+  function compareHands(playerHandValue, houseHandValue) {
     //add a pop up box with result and ask to play again.
     if (playerHandValue === houseHandValue) {
-        return "Draw";
+      return "Draw";
     } else if (houseHandValue === 0) {
-        return "House has black jack! You lose";
+      return "House has black jack! You lose";
     } else if (playerHandValue === 0) {
-        return "You have Blackjack! You Win!";
-    } else if (playerHandValue > 21){
-        return "Your bust! 21 is the limit";
+      return "You have Blackjack! You Win!";
+    } else if (playerHandValue > 21) {
+      return "Your bust! 21 is the limit";
     } else if (houseHandValue > 21) {
-        return "House is bust! You win!";
+      return "House is bust! You win!";
     } else if (playerHandValue > houseHandValue) {
-        return "Congratulations! You beat the house!";
+      return "Congratulations! You beat the house!";
     } else {
-        return "The House wins! Better luck next time!"
+      return "The House wins! Better luck next time!";
     }
-}
+  }
 
-function breakTurn() {
-    return roundOver = true;
-}
-
-
-
+  function breakTurn() {
+    return (roundOver = true);
+  }
+});
